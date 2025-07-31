0349b64a6043e32ba6f0d3524cd5432e6145327f
TanDung382 <tandung382@gmail.com>
2025-07-31 15:07:39 +0700

    Update App.js and App.css (Header/Footer)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 // src/App.js
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';   
+import Footer from './components/Footer';   
 import HomePage from './pages/HomePage';  // Import HomePage component
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />  {/* Hiển thị HomePage */}
       </Routes>
+      <Footer />
     </Router>
   );
 }
