9c9fde05a40a7a5ffe782d6b23e7c08c45a4d58e
Thanhthiennga098 <thanhthiennga098@gmail.com>
2025-07-31 15:19:09 +0700

    Lưu thay đổi local trước khi pull

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';  // Import HomePage component
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <div className="container">
-          <h2>Chào mừng đến với Movie Web!</h2>
-          <p>Đây là nội dung giữa Header và Footer.</p>
-        </div>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />  {/* Hiển thị HomePage */}
+      </Routes>
     </Router>
   );
 }
